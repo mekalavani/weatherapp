577bf7a36863a7f06c85acee15d05530c8b31ecb
Vani Mekala <vani.mekala@hitachids.com>
2026-04-01 17:05:41 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { App } from './app/app';
+
+// bootstrapApplication(App, appConfig)
+//   .catch((err) => console.error(err));
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient(withFetch()) // ✅ FIX HERE
+  ]
+});
